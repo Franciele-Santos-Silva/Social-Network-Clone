be850aff61e624d17505a45efe95ee4cb125811f
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-12-10 12:08:46 -0300

    feat:Header adicionado na pasta componets

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import Header from "@/components/Header";
+
+export const metadata = {
+  title:" Facebook"
+}
+
 export default function Home() {
   return (
     <div>
+      <Header/>
       <h1>Clone Facebook</h1>
     </div>
+    
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        {/* Esquerda */}
+        <div></div>
+
+        {/* Central */}
+        {/* Direita */}
+    </div>
+  )
+}
+
+export default Header
